a828a849b0ed56424eea2926289941d28fd4a4b0
Famper <posipov23@gmail.com>
2022-04-21 17:14:24 +0300

    + Add localization + Fixed code

--- Coffee_Machine.py
+++ Coffee_Machine.py
@@ -1,11 +1,18 @@
 import lib.Virtual_Machine
+import lib.func
+import lib.localization
 
+
+# Создаем класс Виртуальной кофемашины и добавляем локализацию
 CF_Machine = lib.Virtual_Machine.CoffeeMachine()
+localization = lib.func.check_language()
+
+print(lib.localization.succ_local[localization])
 
 while True:
-    command = str(input("Write action (buy, fill, take, remaining, exit):\n"))
+    command = input(lib.localization.menu['main_menu'][localization])
 
-    if command.lower() == "exit":
+    if command.lower() == lib.localization.menu['commands'][localization][4]:
         exit(0)
 
-    CF_Machine.check_command(command)
+    CF_Machine.check_command(command, localization)
